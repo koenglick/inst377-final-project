03f0061ab7eef81528f8d0b5d6b71c11e552ff79
koenglick <kglick@terpmail.umd.edu>
2025-12-19 23:34:03 -0500

    ts2 lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -7,22 +7,17 @@ export default function handler(req, res) {
   }
 
   var url =
-    "https://de.wiktionary.org/w/api.php?action=query&format=json&prop=extracts&exintro=1&explaintext=1&redirects=1&titles=" +
-    encodeURIComponent(lemma) +
-    "&origin=*";
+    "https://de.wiktionary.org/api/rest_v1/page/summary/" +
+    encodeURIComponent(lemma);
 
   fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      var pages = data.query.pages;
-      var pageId = Object.keys(pages)[0];
-      var extract = pages[pageId].extract;
-
       res.status(200).json({
         lemma: lemma,
-        extract: extract || ""
+        extract: data.extract || ""
       });
     })
     .catch(function () {
